d1baccd886aa83f449063362c41574b8e942ccb3
Delichaomoon <deligit.work@gmail.com>
2023-03-09 11:36:45 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require('autoprefixer');
+var mainBowerFiles = require('main-bower-files');
+var browserSync = require('browser-sync').create();
+var minimist = require('minimist');
+var gulpSequence = require('gulp-sequence');
+
+
+var envOptions = {
+  string: 'env',
+  default: {env: 'develop'}
+}
+var options =minimist(process.argv.slice(2),envOptions)
+console.log(options);
+
+gulp.task('clean', function () {
+  return gulp.src(['./.tmp','./public'], {read: false})
+    .pipe($.clean());
+});
+
+
+gulp.task('copyHTML',function(){
+    return gulp.src('./source/**/*.html')
+    .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('jade', function() {
+    // var YOUR_LOCALS = {};
+    gulp.src('./source/**/*.jade')
+      .pipe($.plumber())
+      .pipe($.jade({
+        pretty: true
+        // locals: YOUR_LOCALS
+      }))
+      .pipe(gulp.dest('./public/'))
+      .pipe(browserSync.stream());
+  });
+
+function buildStyles() {
+  return gulp.src('./source/**/*.scss')
+    .pipe($.plumber())
+		.pipe($.sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    //編譯完成
+    .pipe($.postcss([autoprefixer()]))
+    .pipe($.if(options.env === "production", $.minifyCss()))
+		.pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
+};
+  
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('./source/**/*.scss', ['buildStyles']);
+  gulp.watch('./source/**/*.jade',['jade'])
+  gulp.watch('source/**/*.js',['babel'])
+  gulp.watch(['./source/scss/**/*.scss'], function() {  
+    // 直接呼叫 sass 這個 Task
+    gulp.start('buildStyles');
+  });
+};
+
+gulp.task('babel', () =>
+	gulp.src('source/**/*.js')
+		.pipe($.sourcemaps.init())
+		.pipe($.babel({
+			presets: ['@babel/preset-env']
+		}))
+		.pipe($.concat('all.js'))
+		.pipe($.uglify({
+      compress:{
+        drop_console: true
+      }
+    }))
+    .pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('./public/js'))
+    .pipe(browserSync.stream())
+);
+
+gulp.task('bower', function() {
+  return gulp.src(mainBowerFiles({
+    "overrides":{
+      "vue": {
+        "main" :'dist/vue.js'
+      }
+    }
+  }))
+    .pipe(gulp.dest('./.tmp/vendors'))
+});
+gulp.task('vendorJS',['bower'] ,function(){
+  return gulp.src('./.tmp/vendors/**/**.js')
+  .pipe($.order([
+    'jquery.js',
+    'bootstrap.js'
+  ]))
+  .pipe($.concat('vendor.js'))
+  .pipe($.if(options.env === "production",$.uglify()))
+  .pipe(gulp.dest('./public/js'))
+})
+
+gulp.task('image',function() {
+  return gulp.src('./source/img/**/*')
+  .pipe($.if(options.env ==='production', $.imagemin()))
+  .pipe(gulp.dest('./public/img/'));
+});
+
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+        baseDir: "./public/"
+    }
+  });
+});
+
+gulp.task('build', gulpSequence('clean','jade','buildStyles','image','babel','vendorJS'))
+
+
+gulp.task('default',['jade','buildStyles','babel','vendorJS','image','browser-sync','watch']);
